ae52087e9787255e175ec0af0955792e99fc1a46
mjoze <mirek.jozefiak@gmail.com>
2020-07-08 18:27:35 +0200

    add main function

--- battleship/js/app.js
+++ battleship/js/app.js
@@ -35,17 +35,22 @@ class Ships {
     let number = Math.floor(Math.random() * 100);
     let shipCol = 1;
     let shipRow = 10;
-    if (!Boards.shipOnBoards.includes(number)) {
-      console.log("available");
-      for (let i = 0; i < this.length; i++) {
+    console.log("available");
+    for (let i = 0; i < this.length; i++) {
+      if (!Boards.shipOnBoards.includes(number)) {
+        this.position.push(number);
         if (direction === 1) {
-          console.log(1);
+          number += shipCol;
         } else {
-          console.log(0);
+          if (number > 90) {
+            number -= shipCol;
+          } else {
+            number += shipRow;
+          }
         }
+      } else {
+        console.log("error");
       }
-    } else {
-      console.log("error");
     }
     if (this.player === "player") {
       Boards.playerShips.push([this.position]);
@@ -56,8 +61,7 @@ class Ships {
 }
 
 class Controller {
-  // constructor() {}
-  fire(Boards, target) {
+  firePlayer(Boards, target) {
     for (const el of Boards.compShips) {
       for (const i of el) {
         for (const index of i) {
@@ -70,12 +74,54 @@ class Controller {
       }
     }
   }
+  fireComp(Boards) {
+    const target = Math.floor(Math.random() * 100);
+    for (const el of Boards.playerShips) {
+      for (const i of el) {
+        for (const index of i) {
+          if (index === target) {
+            console.log("hit");
+            Boards.boards[target - 1].style.backgroundColor = "black";
+          } else {
+            console.log("miss");
+          }
+        }
+      }
+    }
+  }
 }
+
+const battle = (Boards, Controller) => {
+  console.log("gramy");
+  let game = 5;
+
+  while (game > 0) {
+    let target = Math.floor(Math.random() * 100);
+
+    if (Boards.playerShips.length === 0) {
+      console.log("comp win");
+      return false;
+    }
+    if (Boards.compShips.length === 0) {
+      console.log("player win");
+      return false;
+    }
+    Controller.firePlayer(Boards, target);
+    console.log(target);
+    Controller.fireComp(Boards);
+    game -= 1;
+  }
+};
+
 const boards = new Boards(".ships");
 const s = new Ships(3, boards.boards, [], [], "computer");
-console.log(s.position);
 const s2 = new Ships(4, boards.boards, [], [], "player");
-console.log(s2.position);
 s.generateShip(boards);
+s.displayShip(boards);
 s2.generateShip(boards);
+s2.displayShip(boards);
 const game = new Controller();
+game.fireComp(boards);
+console.log(boards);
+
+battle(boards, game);
