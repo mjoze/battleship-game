3b2153272765e99a7aa13f893ba5b588a716f1fc
mjoze <mirek.jozefiak@gmail.com>
2020-07-13 18:54:58 +0200

    new classes

--- battleship/js/app.js
+++ battleship/js/app.js
@@ -5,11 +5,34 @@ import "../sass/style.scss";
 // ships in the game
 // player's ships
 // computer ships
-
+class Game {
+  constructor(playerShips, computerShips) {
+    this.ships = []
+    this.playerShips = playerShips
+    this.computerShips = computerShips
+  }
+  addShips() {
+    this.ships = [this.playerShips, this.computerShips]
+  }
+}
 // ship
 // location
 // hits
-
+class Ship {
+  constructor(length) {
+    this.length = length
+    this.location = []
+    this.hits = 0
+  }
+}
 // controller
 // result
-// hits
+// fire
+class controller {
+  constructor() {
+    this.result = {}
+
+  }
+  fire() {}
+
+}
